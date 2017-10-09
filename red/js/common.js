3d7999258a99264a2e31797db0d04d135a64ff91
rotterx <q.fq@yandex.ru>
2017-10-09 14:58:44 +0300

    Commit red

---
+++ red/js/common.js
@@ -0,0 +1,27 @@
+
+
+$(function () {
+
+    $('.popup-link').magnificPopup({
+
+        delegate: 'a',
+
+        type: 'image',
+
+        gallery: {
+
+            enabled: true,
+
+        },
+
+        zoom: {
+
+            enabled: true,
+
+            duration: 300
+
+        }
+
+    });
+
+});
---
+++ red/js/preloader.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#about, #skills, #portfolio, #contacts").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+    });
+});

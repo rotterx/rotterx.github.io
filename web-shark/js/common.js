2566e552ba05b31c14020b018942ccfdc09d5435
rotterx <q.fq@yandex.ru>
2017-10-30 22:41:10 +0300

    gvozd

--- web-shark/js/common.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-$('.menu-icon').on('click', function () {
-    $('#nav ul').toggleClass('showing');
-});
-new WOW().init();
-});
--- web-shark/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    
-    $(".filter li").click(function () {
-        $(".filter li").removeClass("active");
-        $(this).addClass("active");
-    });
-    
-});
